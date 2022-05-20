f53177bf3995a45db5b5f298d3ca8274ca3d71b8
Lucky <lucky2077@mailverse.xyz>
2022-05-20 11:26:56 +0800

    fix hardhat.config.js lint error

--- hardhat.config.js
+++ hardhat.config.js
@@ -22,12 +22,12 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity:{
+  solidity: {
     version: "0.8.14",
     settings: {
       optimizer: {
         enabled: true,
-        runs: 800
+        runs: 800,
       },
     },
   },
