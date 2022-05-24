51700a414a868944dc418f6cdca7a88d4ef279d0
reddqian <will@rarilabs.com>
2022-05-24 16:54:34 +0800

    remove unused variables

--- test/gas-test.js
+++ test/gas-test.js
@@ -3,12 +3,12 @@ const { ethers } = require("hardhat");
 const parseEther = ethers.utils.parseEther;
 
 let RA_NFT, contract;
-let account02, account03;
+let owner;
 
 const MINT_PRICE = "0.1";
 
 beforeEach(async () => {
-  [owner, account02, account03] = await ethers.getSigners();
+  [owner] = await ethers.getSigners();
 
   RA_NFT = await ethers.getContractFactory("ERC721RA_NFT");
   contract = await RA_NFT.deploy(0);
@@ -17,92 +17,82 @@ beforeEach(async () => {
 
 describe("Gas Test", function () {
   it("Mint 1x ERC721RA ...", async function () {
-    await contract.connect(account02).mint1x({ value: parseEther(MINT_PRICE) });
-    await contract.connect(account02).mint1x({ value: parseEther(MINT_PRICE) });
-    await contract.connect(account02).mint1x({ value: parseEther(MINT_PRICE) });
-    await contract.connect(account02).mint1x({ value: parseEther(MINT_PRICE) });
-    await contract.connect(account02).mint1x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint1x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint1x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint1x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint1x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint1x({ value: parseEther(MINT_PRICE) });
   });
 
   it("Mint 2x ERC721RA ...", async function () {
-    await contract.connect(account02).mint2x({ value: parseEther(MINT_PRICE) });
-    await contract.connect(account02).mint2x({ value: parseEther(MINT_PRICE) });
-    await contract.connect(account02).mint2x({ value: parseEther(MINT_PRICE) });
-    await contract.connect(account02).mint2x({ value: parseEther(MINT_PRICE) });
-    await contract.connect(account02).mint2x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint2x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint2x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint2x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint2x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint2x({ value: parseEther(MINT_PRICE) });
   });
 
   it("Mint 3x ERC721RA ...", async function () {
-    await contract.connect(account02).mint3x({ value: parseEther(MINT_PRICE) });
-    await contract.connect(account02).mint3x({ value: parseEther(MINT_PRICE) });
-    await contract.connect(account02).mint3x({ value: parseEther(MINT_PRICE) });
-    await contract.connect(account02).mint3x({ value: parseEther(MINT_PRICE) });
-    await contract.connect(account02).mint3x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint3x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint3x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint3x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint3x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint3x({ value: parseEther(MINT_PRICE) });
   });
 
   it("Mint 4x ERC721RA ...", async function () {
-    await contract.connect(account02).mint4x({ value: parseEther(MINT_PRICE) });
-    await contract.connect(account02).mint4x({ value: parseEther(MINT_PRICE) });
-    await contract.connect(account02).mint4x({ value: parseEther(MINT_PRICE) });
-    await contract.connect(account02).mint4x({ value: parseEther(MINT_PRICE) });
-    await contract.connect(account02).mint4x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint4x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint4x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint4x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint4x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint4x({ value: parseEther(MINT_PRICE) });
   });
 
   it("Mint 5x ERC721RA ...", async function () {
-    await contract.connect(account02).mint5x({ value: parseEther(MINT_PRICE) });
-    await contract.connect(account02).mint5x({ value: parseEther(MINT_PRICE) });
-    await contract.connect(account02).mint5x({ value: parseEther(MINT_PRICE) });
-    await contract.connect(account02).mint5x({ value: parseEther(MINT_PRICE) });
-    await contract.connect(account02).mint5x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint5x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint5x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint5x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint5x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint5x({ value: parseEther(MINT_PRICE) });
   });
 
   it("Mint 6x ERC721RA ...", async function () {
-    await contract.connect(account02).mint6x({ value: parseEther(MINT_PRICE) });
-    await contract.connect(account02).mint6x({ value: parseEther(MINT_PRICE) });
-    await contract.connect(account02).mint6x({ value: parseEther(MINT_PRICE) });
-    await contract.connect(account02).mint6x({ value: parseEther(MINT_PRICE) });
-    await contract.connect(account02).mint6x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint6x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint6x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint6x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint6x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint6x({ value: parseEther(MINT_PRICE) });
   });
 
   it("Mint 7x ERC721RA ...", async function () {
-    await contract.connect(account02).mint7x({ value: parseEther(MINT_PRICE) });
-    await contract.connect(account02).mint7x({ value: parseEther(MINT_PRICE) });
-    await contract.connect(account02).mint7x({ value: parseEther(MINT_PRICE) });
-    await contract.connect(account02).mint7x({ value: parseEther(MINT_PRICE) });
-    await contract.connect(account02).mint7x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint7x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint7x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint7x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint7x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint7x({ value: parseEther(MINT_PRICE) });
   });
 
   it("Mint 8x ERC721RA ...", async function () {
-    await contract.connect(account02).mint8x({ value: parseEther(MINT_PRICE) });
-    await contract.connect(account02).mint8x({ value: parseEther(MINT_PRICE) });
-    await contract.connect(account02).mint8x({ value: parseEther(MINT_PRICE) });
-    await contract.connect(account02).mint8x({ value: parseEther(MINT_PRICE) });
-    await contract.connect(account02).mint8x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint8x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint8x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint8x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint8x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint8x({ value: parseEther(MINT_PRICE) });
   });
 
   it("Mint 9x ERC721RA ...", async function () {
-    await contract.connect(account02).mint9x({ value: parseEther(MINT_PRICE) });
-    await contract.connect(account02).mint9x({ value: parseEther(MINT_PRICE) });
-    await contract.connect(account02).mint9x({ value: parseEther(MINT_PRICE) });
-    await contract.connect(account02).mint9x({ value: parseEther(MINT_PRICE) });
-    await contract.connect(account02).mint9x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint9x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint9x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint9x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint9x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint9x({ value: parseEther(MINT_PRICE) });
   });
 
   it("Mint 10x ERC721RA ...", async function () {
-    await contract
-      .connect(account02)
-      .mint10x({ value: parseEther(MINT_PRICE) });
-    await contract
-      .connect(account02)
-      .mint10x({ value: parseEther(MINT_PRICE) });
-    await contract
-      .connect(account02)
-      .mint10x({ value: parseEther(MINT_PRICE) });
-    await contract
-      .connect(account02)
-      .mint10x({ value: parseEther(MINT_PRICE) });
-    await contract
-      .connect(account02)
-      .mint10x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint10x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint10x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint10x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint10x({ value: parseEther(MINT_PRICE) });
+    await contract.connect(owner).mint10x({ value: parseEther(MINT_PRICE) });
   });
 });
